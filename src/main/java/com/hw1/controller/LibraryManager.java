1ff0c2616c109db966fef8ecd8eb02bd585c9b05
jiyeong08 <a01086283142@gmail.com>
2023-08-16 20:01:59 +0900

    20230816 jy

--- src/main/java/com/hw1/controller/LibraryManager.java
+++ src/main/java/com/hw1/controller/LibraryManager.java
@@ -1,23 +1,84 @@
 package com.hw1.controller;
 
+import com.hw1.model.dto.AniBook;
+import com.hw1.model.dto.CookBook;
+
 public class LibraryManager {
 
-    public void insertMember(Member mem(){
+
+
+
+
+
+
+
+
+
+    public void insertMember(Member mem) {
+
+
+
+
+
+
+
+
+
 
     }
 
-    public Member myInfo(){
+    public Member myInfo() {
+
+
+
+
+
+
+
+
+
 
     }
 
-    public Book[] selectAll(){
+    public Book[] selectAll() {
+
+
+
+
+
+
+
+
+
 
     }
 
-    public book[] searchBook(String keyword){
+    public Book[] searchBook(String keyword) {
+        Book[] book = new Book[5];
+        for(
+                if(bList.contains(keyword)){
+                    bList.add();
+                }
+        )
 
+
+
+
+
+
+        return searchList;
     }
 
     public int rentBook(int index) {
 
+
+
+
+
+
+
+
+
+
+    }
 }
--- src/main/java/com/hw1/view/LibraryMenu.java
+++ src/main/java/com/hw1/view/LibraryMenu.java
@@ -1,20 +1,69 @@
 package com.hw1.view;
 
+import com.hw1.controller.LibraryManager;
+
+import java.util.Scanner;
+
 public class LibraryMenu {
 
+
+
+
+
+
+
+
+
+
     public void mainMenu() {
+
+
+
+
+
+
+
+
+
 
     }
 
     public void selectAll(){
 
+
+
+
+
+
+
+
+
+
     }
 
     public void searchBook() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("검색할 제목 키워드 : ");
+        String keyword = sc.nextLine();
+        LibraryManager.searchBook(keyword);
 
+        Book[] searchList = lm.searchBook(keyword);
+
+        for(List books : searchList){
+            System.out.println(books);
+        }
     }
 
     public void rentBook(){
+
+
+
+
+
+
+
+
+
 
     }
 
